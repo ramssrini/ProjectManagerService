6d94a1b93108adcfca3967a4f803da3d6f62eae5
ramssrini <rameshkumarsrinivasan21@gmail.com>
2018-12-30 12:40:38 -0500

    Update Utils.java

--- src/main/java/com/cts/fsd/projectmanager/utils/Utils.java
+++ src/main/java/com/cts/fsd/projectmanager/utils/Utils.java
@@ -1,5 +1,10 @@
 package com.cts.fsd.projectmanager.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -14,6 +19,30 @@ public class Utils {
 	    // Now connect to your databases
 	    DB db = mongoClient.getDB("taskmanagerdb");
 	    DBCollection collection = db.getCollection("counters");
+	    System.out.println("collection.count()>>>>>>>"+collection.count());
+	    if(collection.count() == 0)
+	    {
+	    	Map mapProject = new HashMap<>();
+	    	mapProject.put("_id", "projectid");
+	    	mapProject.put("seq", 0);
+	    	DBObject dbObject = new BasicDBObject();
+	    	collection = db.createCollection("counters",dbObject);
+	    	collection.insert(new BasicDBObject(mapProject));
+	    	Map mapUser = new HashMap<>();
+	    	mapUser.put("_id", "userid");
+	    	mapUser.put("seq", 0);
+	    	collection.insert(new BasicDBObject(mapUser));
+	    	
+	    	Map mapTask = new HashMap<>();
+	    	mapTask.put("_id", "taskid");
+	    	mapTask.put("seq", 0);
+	    	collection.insert(new BasicDBObject(mapTask));
+	    	
+	    	Map mapParentTask = new HashMap<>();
+	    	mapParentTask.put("_id", "parentTaskid");
+	    	mapParentTask.put("seq", 0);
+	    	collection.insert(new BasicDBObject(mapParentTask));
+	    }
 	    BasicDBObject find = new BasicDBObject();
 	    find.put("_id", name);
 	    BasicDBObject update = new BasicDBObject();
